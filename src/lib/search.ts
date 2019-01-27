bc1ba37da3ef3704c0e8eaed0877ab91444d18af
Tim Beyer <tim.beyer@gmail.com>
2019-01-27 19:50:48 +0100

    refactor: use non-recursive solution
    currently this is about 3x slower than before
    hopefully by replacing all the object creation with simple arrays we can speed this up a lot

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -2,7 +2,36 @@ import Column from './column'
 import DataObject from './data-object'
 import { Constraint } from './constraint'
 
-let searchMatrix = function searchMatrix (root: Column, limit: number = Infinity) {
+interface SearchState {
+  neighbor: Column
+  currentColumnHeader: Column
+  dataObject: DataObject
+}
+
+function cover (searchState: SearchState, dataObject: DataObject): void {
+  let neighbor = dataObject.right
+  while (neighbor !== dataObject) {
+    neighbor.columnHeader.cover()
+    neighbor = neighbor.right
+  }
+  searchState.neighbor = neighbor
+}
+
+function uncover (searchState: SearchState, dataObject: DataObject): void {
+
+  let neighbor = searchState.neighbor.left
+
+  while (neighbor !== dataObject) {
+    neighbor.columnHeader.uncover()
+    neighbor = neighbor.left
+  }
+
+  searchState.neighbor = neighbor
+}
+
+type SearchStates = ('enterSearch' | 'goDown' | 'goUp')
+
+let searchMatrix = function searchMatrix (root: Column, limit: number = Infinity): Constraint[][] {
   function chooseColumn () {
     let smallestColumn = root.right
     let currentColumn = root.right
@@ -17,77 +46,96 @@ let searchMatrix = function searchMatrix (root: Column, limit: number = Infinity
     return smallestColumn
   }
 
-  function doSearch () {
-    const objects: DataObject[] = []
-    const results: Constraint[][] = []
-    let stopped = false;
+  const objects: DataObject[] = []
+  const results: Constraint[][] = []
+  let searchState: SearchStates = 'enterSearch'
 
-    (function search (k) {
-      if (root === root.right) {
-        const result = []
+  let stopped = false
 
-        for (let i = 0; i < k; i++) {
-          const o = objects[i]
-          result.push(o.data)
+  let k = 0
+  let stateStack: SearchState[] = []
+  let currentState: SearchState
+
+  mainLoop:
+  while (!stopped) {
+    switch (searchState as SearchStates) {
+      case 'enterSearch': {
+        const isSolution = root === root.right
+
+        if (isSolution) {
+          const result = []
+
+          for (let i = 0; i < k; i++) {
+            const o = objects[i]
+            result.push(o.data)
+          }
+
+          results.push(result)
+
+          if (results.length === limit) {
+            stopped = true
+          }
+
+          searchState = 'goUp'
+        } else {
+          const currentColumnHeader = chooseColumn()
+          const dataObject = currentColumnHeader.down as DataObject
+
+          currentState = {
+            neighbor: undefined,
+            currentColumnHeader,
+            dataObject
+          }
+
+          currentState.currentColumnHeader.cover()
+          searchState = 'goDown'
         }
+        continue mainLoop
+      }
+      case 'goDown': {
+        if (currentState.dataObject !== currentState.currentColumnHeader) {
+          objects[k] = currentState.dataObject as DataObject
 
-        results.push(result)
+          cover(currentState, currentState.dataObject)
 
-        if (results.length === limit) {
+          k = k + 1
+
+          stateStack.push({
+            ...currentState
+          })
+
+          searchState = 'enterSearch'
+        } else {
+          currentState.currentColumnHeader.uncover()
+          searchState = 'goUp'
+        }
+        continue mainLoop
+      }
+      case 'goUp': {
+        k = k - 1
+
+        if (k < 0) {
           stopped = true
+          continue
         }
 
-        return
+        currentState = stateStack.pop()
+
+        currentState.dataObject = objects[k]
+        currentState.currentColumnHeader = currentState.dataObject.columnHeader
+
+        uncover(currentState, currentState.dataObject)
+
+        currentState.dataObject = currentState.dataObject.down as DataObject
+
+        searchState = 'goDown'
+
+        continue mainLoop
       }
-
-      if (stopped) {
-        return
-      }
-
-      let currentColumnHeader: Column
-      let dataObject: DataObject
-      let neighbor: Column
-
-      currentColumnHeader = chooseColumn()
-      currentColumnHeader.cover()
-
-      dataObject = currentColumnHeader.down as DataObject
-
-      while (dataObject !== currentColumnHeader) {
-        objects[k] = dataObject as DataObject
-        neighbor = dataObject.right
-
-        while (neighbor !== dataObject) {
-          neighbor.columnHeader.cover()
-          neighbor = neighbor.right
-        }
-
-        if (!stopped) {
-          search(k + 1)
-        }
-
-        dataObject = objects[k]
-        currentColumnHeader = dataObject.columnHeader
-
-        neighbor = neighbor.left
-
-        while (neighbor !== dataObject) {
-          neighbor.columnHeader.uncover()
-          neighbor = neighbor.left
-        }
-
-        dataObject = dataObject.down as DataObject
-      }
-
-      currentColumnHeader.uncover()
-
-      return
-    })(0)
-
-    return results
+    }
   }
 
-  return doSearch()
+  return results
 }
 
 const find = function (column: Column, numResults: number): Constraint[][] {
